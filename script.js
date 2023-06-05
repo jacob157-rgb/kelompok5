7ea7c3a79e6873c387efc3e8732cf41fcb65c3f2
Jacob Jockey Saputra <jacob@192.168.1.6>
2023-06-05 22:50:58 +0700

    Contact Form Handling

---
+++ script.js
@@ -0,0 +1,31 @@
+(function () {
+  emailjs.init("3dHGHFhvDMh-UJ_fA");
+})();
+window.onload = function () {
+  var btnSend = document.getElementById("btn-send");
+  var btnLoad = document.getElementById("btn-load");
+  var Asuccess = document.getElementById("a-success");
+  var Aerror = document.getElementById("a-error");
+  var form = document.getElementById("contact-form"); // Define form variable inside the event listener function
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    btnLoad.classList.toggle("d-none");
+    btnSend.classList.toggle("d-none");
+    emailjs.sendForm("service_l9suqrm", "template_p43ej3t", this).then(
+      function () {
+        btnLoad.classList.toggle("d-none");
+        btnSend.classList.toggle("d-none");
+        Asuccess.classList.toggle("d-none");
+        form.reset();
+        console.log("SUCCESS!");
+      },
+      function (error) {
+        Aerror.classList.toggle("d-none");
+        btnLoad.classList.toggle("d-none");
+        btnSend.classList.toggle("d-none");
+        form.reset();
+        console.log("FAILED...", error);
+      }
+    );
+  });
+};
